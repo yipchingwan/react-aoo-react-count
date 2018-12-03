686d94298a852b7b3aea258cf95ccfebb898f875
KYLE\Surface <kyle.public10415@gmail.com>
2018-12-03 15:14:29 +0800

    feat: add +button -button and counter function

--- src/App.js
+++ src/App.js
@@ -3,23 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {number : 0}
+  add = ()=>{
+    this.setState({number:this.state.number+1})
+  }
+  sub = ()=>{
+    this.setState({number:this.state.number-1})
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button onClick={this.add}>+</button>
+        {this.state.number}
+        <button onClick={this.sub}>-</button>
+        
       </div>
     );
   }
